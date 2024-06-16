7f1dd2ea2b32551410212bb057e934c01fbfa27e
maddo <moritz-10@web.de>
2024-06-16 22:29:41 +0200

    Text und Logo und Design

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.getElementById("button1").onclick = function (){
+  window.location.href = "../about-me.html"
+}
+document.getElementById("button2").onclick = function (){
+  window.location.href = "../projects.html"
+}
