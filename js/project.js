bc0f35c0d490d0a6515a5a58d0408d33efc0cd0e
maddo <moritz-10@web.de>
2024-07-25 21:28:14 +0200

    junge geh doch

--- js/project.js
+++ js/project.js
@@ -4,5 +4,5 @@ document.getElementById('resetButton').addEventListener('click', function() {
   document.getElementById('name').value = '';
   document.getElementById('email').value = '';
   document.getElementById('message').value = '';
-  document.getElementById('response').innerHTML = ''; // Optional: Auch die Antwortnachricht leeren
+  document.getElementById('response').innerHTML = '';
 });
